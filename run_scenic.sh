348ea3c25de92ba64ec761543d82623b0631f178
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2022-11-25 17:36:04 +0800

    Create run_scenic.sh

---
+++ run_scenic.sh
@@ -0,0 +1,15 @@
+git clone --branch=main https://github.com/google-research/t5x
+cd t5x
+
+python3 -m pip install -e '.[tpu]' -f \
+  https://storage.googleapis.com/jax-releases/libtpu_releases.html
+export PATH=~/.local/bin:$PATH
+
+git clone https://github.com/YOUR_GITHUB_NAME/scenic.git
+cd scenic
+pip install .
+
+export WORK_DIR=gs://YOUR_BUDGET/scenic/mnist
+python3 scenic/main.py \
+  --config=scenic/projects/baselines/configs/mnist/mnist_config.py \
+  --workdir=$WORK_DIR
